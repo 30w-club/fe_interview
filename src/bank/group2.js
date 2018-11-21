b19e31b1ab9d888b5aa92b913d0a0e88e57f7ad6
yinxing <yinxingdyx@163.com>
2018-11-21 17:06:16 +0800

    Update bank.

--- src/bank/group2.js
+++ src/bank/group2.js
@@ -34,5 +34,39 @@ export default [
         如果then中的回调函数返回一个未定状态（pending）的Promise，那么then返回Promise的状态也是未定的，并且它的终态与那个Promise的终态相同；同时，它变为终态时调用的回调函数参数与那个Promise变为终态时的回调函数的参数是相同的。
       `}
     ]
+  },
+  {
+    title: { type: types.plain, val: '使用import时，webpack对node_modules里的依赖会做什么' },
+    desc: [
+      {
+        type: types.plain, val: `三种引入方式
+        相对路径 import {button} from '../component/button'
+        绝对路径 import {button} from '/home/me/file'
+        模块名 import React from 'react'
+        `
+      },
+      {
+        type: types.plain, val: `
+        默认的node_modules可以根据webpack配置的resolve.modules进行更改
+        查找中会根据webpack配置的resolve.extensions自动补全扩展名
+        查找中会根据wepack配置的resolve.alias替换掉别名`
+      },
+      {
+        type: types.plain, val: `
+        webpack解析代码路径的核心模块是enhanced-resolve模块
+        webpack解析匹配代码路径的配置在resolve里面`
+      }
+    ]
+  },
+  {
+    title: { type: types.plain, val: 'js 异步编程' },
+    desc: [
+      {
+        type: types.plain, val: `
+        callback
+        Promise
+        async/await`
+      }
+    ]
   }
 ]
