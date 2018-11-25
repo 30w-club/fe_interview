c988c86a346fcdf91a82cde31589058ba632c525
yinxing <yinxingdyx@163.com>
2018-11-25 14:51:06 +0800

    Add content in Create Page.

--- src/pages/Create.vue
+++ src/pages/Create.vue
@@ -1,30 +1,82 @@
 <template lang="pug">
   .create
+    .home(@click="goToHome") Home
+    .title
+      select(v-model='titleType')
+        option(v-for='type in types') {{type}}
+      input(type="text" v-model='titleVal' placeholder='Title')
+    .desc
+      .desc_item(v-for='item in desc')
+        select(v-model='item.type')
+          option(v-for='type in types') {{type}}
+        textarea(placeholder='Desc' v-model="item.val")
+      .add(@click="addDesc") +
     .click(@click="addItem") add
 </template>
 
 <script>
+import types from '../bank/TYPES'
+
 export default {
   name: 'Create',
+  data () {
+    return {
+      titleVal: '',
+      titleType: types.plain,
+      types: {},
+      desc: [
+        {
+          type: types.plain,
+          val: ''
+        }
+      ]
+    }
+  },
+  created () {
+    this.types = {...types}
+  },
   mounted () {
     console.log('mounted')
   },
   methods: {
     async addItem () {
-      const resp = await this.$idb.add({
+      let bankItem = {
         title: {
-          type: 'plain',
-          val: 'test'
+          type: this.titleType,
+          val: this.titleVal
         },
-        desc: [
-          {
-            type: 'plain',
-            val: 'test'
-          }
-        ]
+        desc: [...this.desc]
+      }
+      const desc = this.desc
+        .filter(item => item.val)
+        .map(item => ({type: item.type, val: item.val}))
+      bankItem.desc = [...desc]
+      if (bankItem.title.val && bankItem.desc[0].val) {
+        const resp = await this.$idb.add(bankItem)
+        console.log(resp)
+      }
+    },
+    addDesc () {
+      this.desc.push({
+        type: types.plain,
+        val: ''
       })
-      console.log(resp)
+    },
+    goToHome () {
+      this.$router.push({ name: 'Home' })
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.create {
+  .desc {
+    .desc_item {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+}
+</style>
